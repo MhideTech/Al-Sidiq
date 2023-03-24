d2527e7e6f87f9e0db32a1190e7a6eb34d7bb4f2
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-03-24 18:53:22 +0100

    modified:   index.html 	modified:   static/javascript/script.js

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,13 +1,14 @@
 const nav = document.querySelector("nav")
-const active = document.querySelector("navbar-active")
+const active = document.querySelector(".nav-active")
+const active2 = document.querySelector(".nav-active2")
 document.addEventListener("scroll", function(){
-    if(window.scrollY > 174){
+    if(window.scrollY > 100){
         nav.style.backgroundColor = "#1CB9C8";
         nav.style.width = "100%";
         nav.style.height = "fit-content";
         nav.style.margin = "0px auto"
         active.style.color = "#00122D"
-        active.style.color = "#fff"
+        active2.style.color = "#00122D"
     } else{
         nav.style.width = "100%"
         nav.style.backgroundColor = "transparent"
